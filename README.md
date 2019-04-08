1de5b1139fe803f450c2795c84d3ee7ec382b74e
tsampy <tsampy@hotmail.com>
2019-04-08 18:30:10 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# robotStore
+
+Online robot shop store.
+
+Front-end : ReactJS application.
+
+Back-end : Java application using Spring Boot,
+REST web service,
+JSON request,
+Data accessed via Java Persistence API.
+
+Cloud platform : Heroku
---
+++ src/test/java/com/robotstore/robotstore/RobotstoreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.robotstore.robotstore;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RobotstoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
