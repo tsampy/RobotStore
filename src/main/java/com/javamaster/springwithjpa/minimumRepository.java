71f98fab180a7d85ce0adeba483b1651429eb9ad
Thiéry SAMPY <tsampy@hotmail.com>
2019-04-12 14:29:37 +0200

    Delete minimumRepository.java

--- src/main/java/com/javamaster/springwithjpa/minimumRepository.java
+++
@@ -1,6 +0,0 @@
-package com.javamaster.springwithjpa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface minimumRepository extends JpaRepository<minimumInfo, Long> {
-}
