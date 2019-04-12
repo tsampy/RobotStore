3648c44cb76d863aeac9cc3705693463597f8c62
Thiéry SAMPY <tsampy@hotmail.com>
2019-04-12 14:29:16 +0200

    Delete UsersRepository.java

--- src/main/java/com/javamaster/springwithjpa/UsersRepository.java
+++
@@ -1,6 +0,0 @@
-package com.javamaster.springwithjpa;
-
-        import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsersRepository extends JpaRepository<Users, Long> {
-}
